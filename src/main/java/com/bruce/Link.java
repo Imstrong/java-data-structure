519cd5edc8637539e4baefe283f53c2e86a29c79
装逼之王 <zbzwcode@163.com>
2018-05-24 15:59:36 +0800

    二叉搜索树

---
+++ src/main/java/com/bruce/Link.java
@@ -0,0 +1,22 @@
+package com.bruce;
+
+/**
+ * 链表
+ */
+class LinkNode {
+    public int data;
+    public LinkNode next;
+    public void append(LinkNode node){
+
+    }
+}
+class DoubleLinkNode{
+    public int data;
+    public DoubleLinkNode prev;
+    public DoubleLinkNode next;
+}
+public class Link{
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/bruce/SearchTreeNode.java
@@ -0,0 +1,12 @@
+package com.bruce;
+
+public class SearchTreeNode {
+    //关键字、键值
+    private int key;
+    //左子节点
+    private SearchTreeNode left;
+    //右子节点
+    private SearchTreeNode right;
+    //父节点
+    private SearchTreeNode parent;
+}
